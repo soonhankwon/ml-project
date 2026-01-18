99d61186f9f4fbaec8d6a23f0aab1cb77bd4e5c9
soonhankwon <soonable@naver.com>
2026-01-18 09:17:27 +0900

    pandas dataframe

---
+++ 1_pandas/pandas_dataframe_api.py
@@ -0,0 +1,32 @@
+import pandas as pd
+
+titanic_df = pd.read_csv('./titanic_ml_dataset/Titanic_train.csv', sep=',')
+print('titanic 변수 type:', type(titanic_df))
+# titanic 변수 type: <class 'pandas.core.frame.DataFrame'>
+print(titanic_df)
+"""
+     PassengerId  Survived  Pclass  ...     Fare Cabin  Embarked
+0              1         0       3  ...   7.2500   NaN         S
+1              2         1       1  ...  71.2833   C85         C
+2              3         1       3  ...   7.9250   NaN         S
+3              4         1       1  ...  53.1000  C123         S
+4              5         0       3  ...   8.0500   NaN         S
+..           ...       ...     ...  ...      ...   ...       ...
+886          887         0       2  ...  13.0000   NaN         S
+887          888         1       1  ...  30.0000   B42         S
+888          889         0       3  ...  23.4500   NaN         S
+889          890         1       1  ...  30.0000  C148         C
+890          891         0       3  ...   7.7500   NaN         Q
+
+[891 rows x 12 columns]
+"""
+
+print(titanic_df.head(3))
+"""
+   PassengerId  Survived  Pclass  ...     Fare Cabin  Embarked
+0            1         0       3  ...   7.2500   NaN         S
+1            2         1       1  ...  71.2833   C85         C
+2            3         1       3  ...   7.9250   NaN         S
+"""
+
+print()
