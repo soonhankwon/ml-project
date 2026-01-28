133ac6a19c3a8259fe6c4ab98c78a59deefb9162
soonhankwon <soonable@naver.com>
2026-01-28 18:15:08 +0900

    nunique and replace

--- 1_pandas/pandas_missing_data.py
+++ 1_pandas/pandas_missing_data.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 # Missing Data 처리하기
 titanic_df = pd.read_csv('./titanic_ml_dataset/Titanic_train.csv', sep=',')
@@ -50,4 +51,71 @@ Fare           0
 Cabin          0
 Embarked       0
 dtype: int64
+"""
+
+# nunique로 컬럼내 몇건의 고유값이 있는지 파악
+print(titanic_df['Name'].value_counts())
+print(titanic_df['Pclass'].nunique())
+print(titanic_df['Survived'].nunique())
+print(titanic_df['Name'].nunique())
+"""
+Name
+Braund, Mr. Owen Harris                     1
+Boulos, Mr. Hanna                           1
+Frolicher-Stehli, Mr. Maxmillian            1
+Gilinski, Mr. Eliezer                       1
+Murdlin, Mr. Joseph                         1
+                                           ..
+Kelly, Miss. Anna Katherine "Annie Kate"    1
+McCoy, Mr. Bernard                          1
+Johnson, Mr. William Cahoone Jr             1
+Keane, Miss. Nora A                         1
+Dooley, Mr. Patrick                         1
+Name: count, Length: 891, dtype: int64
+
+3
+2
+891
+"""
+
+# replece로 원본 값을 특정값으로 대체
+replace_test_df = pd.read_csv('./titanic_ml_dataset/Titanic_train.csv', sep=',')
+replace_test_df['Sex'].replace('male', 'Man')
+replace_test_df['Sex'] = replace_test_df['Sex'].replace({'male': 'Man', 'female': 'Woman'})
+print(replace_test_df['Sex'].head(3))
+"""
+0      Man
+1    Woman
+2    Woman
+"""
+print(replace_test_df['Cabin'].value_counts(dropna=False))
+"""
+Cabin
+NaN            687
+C23 C25 C27      4
+G6               4
+B96 B98          4
+C22 C26          3
+              ... 
+E34              1
+C7               1
+C54              1
+E36              1
+C148             1
+"""
+replace_test_df['Cabin'] = replace_test_df['Cabin'].replace(np.nan, 'C001')
+print(replace_test_df['Cabin'].value_counts(dropna=False))
+"""
+Cabin
+C001           687
+C23 C25 C27      4
+G6               4
+B96 B98          4
+C22 C26          3
+              ... 
+E34              1
+C7               1
+C54              1
+E36              1
+C148             1
 """
