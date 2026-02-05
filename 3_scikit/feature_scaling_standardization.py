fd5f9098f51c3a5704f23e3790d9169a12072a23
soonhankwon <soonable@naver.com>
2026-02-05 20:18:53 +0900

    feature scaling and standardization

--- 3_scikit/feature_scaling_standardization.py
+++ 3_scikit/feature_scaling_standardization.py
@@ -31,9 +31,110 @@ from sklearn.preprocessing import StandardScaler
 # StandardScaler 객체 생성
 scaler = StandardScaler()
 # StandardScaler로 데이터 셋 변환, fit()과 transform() 호출
-scaler.fit(iris_df)
-iris_scaled = scaler.transform(iris_df)
+# scaler.fit(iris_df)
+# iris_scaled = scaler.transform(iris_df)
+iris_scaled = scaler.fit_transform(iris_df)
 
 # transform()시 scale 변환된 데이터 셋이 numpy ndarray로 반환되어 이를 DataFrame으로 변환
 iris_df_scaled = pd.DataFrame(data=iris_scaled, columns=iris.feature_names)
-print(iris_df_scaled.head(3))
+print('feature 들의 평균 값')
+print(iris_df_scaled.mean())
+print('\nfeature 들의 분산 값')
+print(iris_df_scaled.var())
+"""
+feature 들의 평균 값
+sepal length (cm)   -4.736952e-16
+sepal width (cm)    -7.815970e-16
+petal length (cm)   -4.263256e-16
+petal width (cm)    -4.736952e-16
+dtype: float64
+
+feature 들의 분산 값
+sepal length (cm)    1.006711
+sepal width (cm)     1.006711
+petal length (cm)    1.006711
+petal width (cm)     1.006711
+dtype: float64
+"""
+
+from sklearn.preprocessing import MinMaxScaler
+
+# MinMaxScaler객체 생성
+scaler = MinMaxScaler()
+# MinMaxScaler로 데이터 셋 변환, fit()과 transform() 호출
+scaler.fit(iris_df)
+iris_scaled = scaler.transform(iris_df)
+iris_df_scaled = pd.DataFrame(data=iris_scaled, columns=iris.feature_names)
+print('feature들의 최소 값')
+print(iris_df_scaled.min())
+print('\nfeature들의 최대 값')
+print(iris_df_scaled.max())
+"""
+feature들의 최소 값
+sepal length (cm)    0.0
+sepal width (cm)     0.0
+petal length (cm)    0.0
+petal width (cm)     0.0
+dtype: float64
+
+feature들의 최대 값
+sepal length (cm)    1.0
+sepal width (cm)     1.0
+petal length (cm)    1.0
+petal width (cm)     1.0
+dtype: float64
+"""
+
+# Scaler를 이용하여 학습 데이터와 테스트 데이터에 fit(), transform(), fit_transform() 유의사항
+
+import numpy as np
+# 학습 데이터는 0 부터 10까지, 테스트 데이터는 0부터 5까지 값을 가지는 데이터 세트
+# Scaler 클래스의 fit(), transform()은 2차원 이상 데이터만 가능하므로 reshape(-1,1)로 차원 변경
+train_array = np.arange(0, 11).reshape(-1,1)
+test_array = np.arange(0, 6).reshape(-1,1)
+
+# 최소값 0, 최대값 1로 변환하는 MinMaxScaler 객체 생성
+scaler = MinMaxScaler()
+# fit하게 되면 train_array 데이터의 최소값이 0, 최대값이 10으로 설정
+scaler.fit(train_array)
+# 1/10 scale로 train_array 데이터 변환함. 원본 10 -> 1로 변환됨
+train_scaled = scaler.transform(train_array)
+
+print('원본 train_array 데이터:', np.round(train_array.reshape(-1), 2))
+print('Scale된 train_array 데이터:', np.round(train_scaled.reshape(-1), 2))
+"""
+원본 train_array 데이터: [ 0  1  2  3  4  5  6  7  8  9 10]
+Scale된 train_array 데이터: [0.  0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1. ]
+"""
+
+# 앞에서 생성한 MinMaxScaler에 test_array를 fit()하게 되면 원본 데이터의 최소값이 0, 최대값이 5으로 설정됨
+scaler.fit(test_array)
+# 1/5 scale로 test_array 데이터 변환함. 원본 5->1로 변환
+test_scaled = scaler.transform(test_array)
+# test_array 변환 출력
+print('원본 test_array 데이터: ', np.round(test_array.reshape(-1), 2))
+print('Scale된 test_array 데이터: ', np.round(test_scaled.reshape(-1), 2))
+"""
+원본 test_array 데이터:  [0 1 2 3 4 5]
+Scale된 test_array 데이터:  [0.  0.2 0.4 0.6 0.8 1. ]
+"""
+# 학습할때와, 테스트할때 척도가 달라지는 문제(주의!)
+
+scaler =MinMaxScaler()
+scaler.fit(train_array)
+train_scaled = scaler.transform(train_array)
+print('원본 train_array 데이터:', np.round(train_array.reshape(-1), 2))
+print('Scale된 test_array 데이터:', np.round(test_scaled.reshape(-1), 2))
+"""
+원본 train_array 데이터: [ 0  1  2  3  4  5  6  7  8  9 10] <- 학습 데이터와 테스트 데이터의 척도가 달라짐
+Scale된 test_array 데이터: [0.  0.2 0.4 0.6 0.8 1. ]
+"""
+
+# test_array에 Scale 변환을 할 때는 반드시 fit()을 호출하지 않고 transform() 만으로 변환해야 함.
+test_scaled = scaler.fit_transform(test_array)
+print('\n원본 test_array 데이터:', np.round(test_array.reshape(-1), 2))
+print('Scale된 test_array 데이터:', np.round(test_scaled.reshape(-1), 2))
+"""
+원본 test_array 데이터: [0 1 2 3 4 5]
+Scale된 test_array 데이터: [0.  0.2 0.4 0.6 0.8 1. ]
+"""
