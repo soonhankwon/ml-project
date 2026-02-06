a2a1fe794e9ce5a22b836ab5f7a98f5f7592b5a6
soonhankwon <soonable@gmail.com>
2026-02-06 15:56:41 +0900

    titanic ml model

--- 3_scikit/titanic_ml_model.py
+++ 3_scikit/titanic_ml_model.py
@@ -1,3 +1,4 @@
+from random import Random
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -209,3 +210,147 @@ print(titanic_df.head())
 
 [5 rows x 12 columns]
 """
+
+# Null 처리 함수
+def fillna(df):
+    df['Age'] = df['Age'].fillna(df['Age'].mean())
+    df['Cabin'] = df['Cabin'].fillna('N')
+    df['Embarked'] = df['Embarked'].fillna('N')
+    return df
+
+# 머신러닝 알고리즘에 불필요한 피처 제거
+def drop_features(df):
+    df = df.drop(['PassengerId', 'Name', 'Ticket'], axis=1)
+    return df
+
+# 레이블 인코딩 수행
+def format_features(df):
+    df['Cabin'] = df['Cabin'].str[:1]
+    features = ['Cabin', 'Sex', 'Embarked']
+    for feature in features:
+        le = LabelEncoder()
+        le = le.fit(df[feature])
+        df[feature] = le.transform(df[feature])
+    return df
+
+# 앞에서 설정한 Data Preprocessing 함수 호출
+def transform_features(df):
+    df = fillna(df)
+    df = drop_features(df)
+    df = format_features(df)
+    return df
+
+# 원본 데이터를 재로딩 하고, feature 데이터 셋과 Label 데이터 셋 추출
+titanic_df = pd.read_csv('./titanic_ml_dataset/Titanic_train.csv')
+y_titanic_df = titanic_df['Survived']
+X_titanic_df = titanic_df.drop('Survived', axis=1, inplace=False)
+
+X_titanic_df = transform_features(X_titanic_df)
+print(X_titanic_df.head())
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X_titanic_df, y_titanic_df, 
+test_size=0.2, random_state=11)
+
+print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
+# (712, 8) (179, 8) (712,) (179,)
+
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
+# 결정트리, Random Forest, 로지스틱 회귀를 위한 사이킷런 Classifier 클래스 생성
+dt_clf = DecisionTreeClassifier(random_state=11)
+rf_clf = RandomForestClassifier(random_state=11)
+lr_clf = LogisticRegression(solver='liblinear')
+
+# DecisionTreeClassifier 학습/예측/평가
+dt_clf.fit(X_train, y_train)
+dt_pred = dt_clf.predict(X_test)
+print('DecisionTreeClassifier 정확도: {0:.4f}'.format(accuracy_score(y_test, dt_pred)))
+
+# RandomForestClassifier 학습/예측/평가
+rf_clf.fit(X_train, y_train)
+rf_pred = rf_clf.predict(X_test)
+print('RandomForestClassifier 정확도: {0:.4f}'.format(accuracy_score(y_test, rf_pred)))
+
+# LogisticRegression 학습/예측/평가
+lr_clf.fit(X_train, y_train)
+lr_pred = lr_clf.predict(X_test)
+print('LogisticRegression 정확도: {0:.4f}'.format(accuracy_score(y_test, lr_pred)))
+"""
+DecisionTreeClassifier 정확도: 0.7877
+RandomForestClassifier 정확도: 0.8547
+LogisticRegression 정확도: 0.8659
+"""
+
+from sklearn.model_selection import KFold
+
+def exec_kfold(clf, folds=5):
+    # 폴드 세트를 5개인 KFold객체를 생성, 폴드 수 만큼 예측결과 저장을 위한 리스트 객체 생성
+    kfold = KFold(n_splits=folds)
+    scores = []
+
+    # KFold 교차 검증 수행
+    for iter_count, (train_index, test_index) in enumerate(kfold.split(X_titanic_df)):
+        # X_titanic_df 데이터에서 교차 검증별로 학습과 검증 데이터를 가리키는 index 생성
+        X_train, X_test = X_titanic_df.values[train_index], X_titanic_df.values[test_index]
+        y_train, y_test = y_titanic_df.values[train_index], y_titanic_df.values[test_index]
+
+        # Classifier 학습, 예측, 정확도 계산
+        clf.fit(X_train, y_train)
+        predictions = clf.predict(X_test)
+        accuracy = accuracy_score(y_test, predictions)
+        scores.append(accuracy)
+        print('교차 검증 {0} 정확도: {1:.4f}'.format(iter_count, accuracy))
+    
+    # 5개 fold에서의 평균 정확도 계산
+    mean_score = np.mean(scores)
+    print('평균 정확도: {0:.4f}'.format(mean_score))
+
+exec_kfold(dt_clf, folds=5)
+"""
+교차 검증 0 정확도: 0.7542
+교차 검증 1 정확도: 0.7809
+교차 검증 2 정확도: 0.7865
+교차 검증 3 정확도: 0.7697
+교차 검증 4 정확도: 0.8202
+평균 정확도: 0.7823
+"""
+
+from sklearn.model_selection import cross_val_score
+
+scores = cross_val_score(dt_clf, X_titanic_df, y_titanic_df, cv=5)
+for iter_count, accuracy in enumerate(scores):
+    print('교차 검증 {0} 정확도: {1:.4f}'.format(iter_count, accuracy))
+
+print('평균 정확도: {0:.4f}'.format(np.mean(scores)))
+"""
+교차 검증 0 정확도: 0.7430
+교차 검증 1 정확도: 0.7753
+교차 검증 2 정확도: 0.7921
+교차 검증 3 정확도: 0.7865
+교차 검증 4 정확도: 0.8427
+평균 정확도: 0.7879
+"""
+
+from sklearn.model_selection import GridSearchCV
+
+parameters = {
+    'max_depth': [2,3,5,10],
+    'min_samples_split': [2,3,5],
+    'min_samples_leaf': [1,5,8]
+}
+
+grid_dclf = GridSearchCV(dt_clf, param_grid=parameters, scoring='accuracy', cv=5)
+grid_dclf.fit(X_train, y_train)
+
+print('GridSearchCV 최적 하이퍼 파라미터: ', grid_dclf.best_params_)
+print('GridSearchCV 최고 정확도: {0:.4f}'.format(grid_dclf.best_score_))
+best_dclf = grid_dclf.best_estimator_
+
+# GridSearchCV의 최적 하이퍼 파라미터로 학습된 Estimator로 예측 및 평가 수행
+dpredictions = best_dclf.predict(X_test)
+accuracy = accuracy_score(y_test, dpredictions)
+print('테스트 세트에서의 DeicisionTreeClassifier 정확도: {0:.4f}'.format(accuracy))
