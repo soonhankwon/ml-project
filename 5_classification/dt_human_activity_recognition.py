e441cee7b41304cab7a6d4f49dbfa55a42e0db2d
soonhankwon <soonable@naver.com>
2026-02-21 10:56:01 +0900

    human activity recognition

--- 5_classification/dt_human_activity_recognition.py
+++ 5_classification/dt_human_activity_recognition.py
@@ -100,3 +100,89 @@ print('DecisionTreeClassifier 기본 하이퍼 파리미터: \n', dt_clf.get_par
 DecisionTreeClassifier 기본 하이퍼 파리미터: 
  {'ccp_alpha': 0.0, 'class_weight': None, 'criterion': 'gini', 'max_depth': None, 'max_features': None, 'max_leaf_nodes': None, 'min_impurity_decrease': 0.0, 'min_samples_leaf': 1, 'min_samples_split': 2, 'min_weight_fraction_leaf': 0.0, 'monotonic_cst': None, 'random_state': 156, 'splitter': 'best'}
 """
+
+from sklearn.model_selection import GridSearchCV
+
+params = {
+    'max_depth' : [6, 8, 10, 12, 16, 20, 24],
+    'min_samples_split' : [16]
+}
+
+grid_cv = GridSearchCV(dt_clf, param_grid=params, scoring='accuracy', cv=5, verbose=1)
+grid_cv.fit(X_train, y_train)
+print(f'GridSearchCV 최고 평균 정확도 수치:{grid_cv.best_score_:.4f}')
+print('GridSearchCV 최적 하이퍼 파라미터', grid_cv.best_params_)
+"""
+Fitting 5 folds for each of 7 candidates, totalling 35 fits
+GridSearchCV 최고 평균 정확도 수치:0.8549
+GridSearchCV 최적 하이퍼 파라미터 {'max_depth': 8, 'min_samples_split': 16}
+"""
+
+# GridSearchCV 객체의 cv_results_ 속성을 DataFrame으로 생성
+cv_results_df = pd.DataFrame(grid_cv.cv_results_)
+# max_depth 파라미터 값과 그때의 테스트(Evaluation)셋, 학습 데이터 셋의 정확도 수치 추출
+print(cv_results_df[['param_max_depth', 'mean_test_score']])
+"""
+   param_max_depth  mean_test_score
+0                6         0.847662
+1                8         0.854879
+2               10         0.852705
+3               12         0.845768
+4               16         0.847127
+5               20         0.848624
+6               24         0.848624
+"""
+
+max_depths = [6, 8, 10, 12, 16, 20, 24]
+# max_depth 값을 변화시키면서 그때마다 학습과 테스트셋에서의 예측 성능 측정
+for depth in max_depths:
+    dt_clf = DecisionTreeClassifier(max_depth=depth, min_samples_split=16, random_state=156)
+    dt_clf.fit(X_train, y_train)
+    pred = dt_clf.predict(X_test)
+    accuracy = accuracy_score(y_test, pred)
+    print(f'max_depth = {depth} 정확도: {accuracy:.4f}')
+
+"""
+max_depth = 6 정확도: 0.8551
+max_depth = 8 정확도: 0.8717
+max_depth = 10 정확도: 0.8599
+max_depth = 12 정확도: 0.8571
+max_depth = 16 정확도: 0.8599
+max_depth = 20 정확도: 0.8565
+max_depth = 24 정확도: 0.8565
+"""
+
+params = {
+    'max_depth' : [8, 12, 16, 20],
+    'min_samples_split': [16, 24] 
+}
+
+grid_cv = GridSearchCV(dt_clf, param_grid=params, scoring='accuracy', cv=5, verbose=1)
+grid_cv.fit(X_train, y_train)
+print(f'GridSearchCV 최고 평균 정확도 수치:{grid_cv.best_score_:.4f}')
+print('GridSearchCV 최적 하이퍼 파라미터', grid_cv.best_params_)
+
+best_df_clf = grid_cv.best_estimator_
+pred1 = best_df_clf.predict(X_test)
+accuracy = accuracy_score(y_test, pred1)
+print(f'결정 트리 예측 정확도:{accuracy:.4f}')
+
+"""
+Fitting 5 folds for each of 8 candidates, totalling 40 fits
+GridSearchCV 최고 평균 정확도 수치:0.8549
+GridSearchCV 최적 하이퍼 파라미터 {'max_depth': 8, 'min_samples_split': 16}
+결정 트리 예측 정확도:0.8717
+"""
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+ftr_importances_values = best_df_clf.feature_importances_
+# Top 중요도로 정렬을 쉽게하고, Seaborn의 막대그래프로 쉽게 표현하기 위해 Series 변환
+ftr_importances = pd.Series(ftr_importances_values, index=X_train.columns)
+# 중요도값 순으로 Series 정렬
+ftr_top20 = ftr_importances.sort_values(ascending=False)[:20]
+plt.figure(figsize=(8,6))
+plt.title('Feature importances Top 20')
+sns.barplot(x=ftr_top20, y=ftr_top20.index)
+plt.show()
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
 def main():
     print("Hello from ml-project!")
-
+    array1 = np.array([1, 2, 3, 4, 5])
+    print(array1.shape())
 
 if __name__ == "__main__":
     main()
