16184682590939732fbfb0b1edf04dd92b0aee6e
soonhankwon <soonable@gmail.com>
2026-03-17 16:38:21 +0900

    stacking model with cross validation

--- 5_classification/stacking_model.py
+++ 5_classification/stacking_model.py
@@ -72,3 +72,56 @@ print(f'최종 메타 모델의 예측 정확도: {accuracy_score(y_test, final)
 """
 최종 메타 모델의 예측 정확도: 0.9737
 """
+
+# CV 세트 기반의 스태킹
+from sklearn.model_selection import KFold
+from sklearn.metrics import mean_absolute_error
+
+# 개별 기반 모델에서 최종 메타 모델이 사용할 학습 및 테스트용 데이터를 생성하기 위한 함수
+def get_stacking_base_datasets(model, X_train_n, y_train_n, X_test_n, n_folds):
+    # 지정된 n_folds 값으로 KFold 생성
+    kf = KFold(n_splits=n_folds, shuffle=False)
+    # 추후에 메타모델이 사용할 학습 데이터 반환을 위한 넘파이 배열 초기화
+    train_fold_pred = np.zeros((X_train_n.shape[0], 1))
+    test_pred = np.zeros((X_test_n.shape[0], n_folds))
+    print(model.__class__.__name__, 'model 시작 ')
+
+    for folder_counter, (train_index, valid_index) in enumerate(kf.split(X_train_n)):
+        # 입력된 학습데이터에서 기반 모델이 학습/예측할 폴드 데이터셋 추출
+        print('\t 폴드 세트: ', folder_counter, ' 시작 ')
+        X_tr = X_train_n[train_index]
+        y_tr = y_train_n[train_index]
+        X_te = X_train_n[valid_index]
+
+        # 폴드 세트 내부에서 다시 만들어진 학습 데이터로 기반 모델의 학습 수행
+        model.fit(X_tr, y_tr)
+        # 폴드 세트 내부에서 다시 만들어진 검증 데이터로 기반 모델 예측 후 데이터 저장
+        train_fold_pred[valid_index, :] = model.predict(X_te).reshape(-1,1)
+        # 입력된 원본 테스트 데이터를 폴드 세트내 학습된 기반 모델에서 예측 후 데이터 저장
+        test_pred[:, folder_counter] = model.predict(X_test_n)
+
+    # 폴드 세트 내에서 원본 테스트 데이터를 예측한 데이터를 평균하여 테스트 데이터로 생성
+    test_pred_mean = np.mean(test_pred, axis=1).reshape(-1, 1)
+
+    # train_fold_pred는 최종 메타 모델이 사용하는 학습 데이터, test_pred_mean은 테스트 데이터
+    return train_fold_pred, test_pred_mean
+
+knn_train, knn_test = get_stacking_base_datasets(knn_clf, X_train, y_train, X_test, 7)
+rf_train, rf_test = get_stacking_base_datasets(rf_clf, X_train, y_train, X_test, 7)
+dt_train, dt_test = get_stacking_base_datasets(dt_clf, X_train, y_train, X_test,  7)    
+ada_train, ada_test = get_stacking_base_datasets(ada_clf, X_train, y_train, X_test, 7)
+
+Stack_final_X_train = np.concatenate((knn_train, rf_train, dt_train, ada_train), axis=1)
+Stack_final_X_test = np.concatenate((knn_test, rf_test, dt_test, ada_test), axis=1)
+print('원본 학습 피처 데이터 Shape:', X_train.shape, '원본 테스트 피처 Shape:', X_test.shape)
+print('스태킹 학습 피처 데이터 Shape:', Stack_final_X_train.shape, '스태킹 테스트 피처 데이터 Shape:', Stack_final_X_test.shape)
+
+lr_final.fit(Stack_final_X_train, y_train)
+stack_final = lr_final.predict(Stack_final_X_test)
+
+print(f'최종 메타 모델의 예측 정확도: {accuracy_score(y_test, stack_final):.4f}')
+"""
+원본 학습 피처 데이터 Shape: (455, 30) 원본 테스트 피처 Shape: (114, 30)
+스태킹 학습 피처 데이터 Shape: (455, 4) 스태킹 테스트 피처 데이터 Shape: (114, 4)
+최종 메타 모델의 예측 정확도: 0.9649
+"""
