b1d44b9d00b87e93d324eadcb2eaa0a98de274cb
soonhankwon <soonable@gmail.com>
2026-05-13 16:31:54 +0900

    kmeans

--- 8_clustering/kmeans.py
+++ 8_clustering/kmeans.py
@@ -1,3 +1,4 @@
+from scipy import cluster
 from sklearn.preprocessing import scale
 from sklearn.datasets import load_iris
 from sklearn.cluster import KMeans
@@ -63,3 +64,56 @@ print(irisDF.head(3))
 1           4.9          3.0           1.4          0.2       0        1 -2.714142 -0.177001
 2           4.7          3.2           1.3          0.2       0        1 -2.888991 -0.144949
 """
+
+# 군집값이 0, 1, 2인 경우마다 별도의 인덱스로 추출
+marker0_ind = irisDF[irisDF['cluster'] == 0].index
+marker1_ind = irisDF[irisDF['cluster'] == 1].index
+marker2_ind = irisDF[irisDF['cluster'] == 2].index
+
+# 군집값이 0, 1, 2에 해당하는 인덱스로 각 군집 레벨의 pca_x, pca_y값 추출. o, s, ^로 마커표시
+plt.scatter(x=irisDF.loc[marker0_ind, 'pca_x'], y=irisDF.loc[marker0_ind, 'pca_y'], marker='o')
+plt.scatter(x=irisDF.loc[marker1_ind, 'pca_x'], y=irisDF.loc[marker1_ind, 'pca_y'], marker='s')
+plt.scatter(x=irisDF.loc[marker2_ind, 'pca_x'], y=irisDF.loc[marker2_ind, 'pca_y'], marker='^')
+
+plt.xlabel('PCA 1')
+plt.ylabel('PCA 2')
+plt.title('3 Clusters Visualization by 2 PCA Components')
+plt.show()
+
+# Clustering 알고리즘 테스트를 위한 데이터 생성
+from sklearn.datasets import make_blobs
+
+X, y = make_blobs(n_samples=200, n_features=2, centers=3, cluster_std=0.8, random_state=0)
+print(X.shape, y.shape)
+"""
+(200, 2) (200,)
+"""
+
+# y target 값의 분포 확인
+unique, counts = np.unique(y, return_counts=True)
+print(unique, counts)
+"""
+[0 1 2] [67 67 66]
+"""
+
+clusterDF = pd.DataFrame(data=X, columns=['ftr1', 'ftr2'])
+clusterDF['target'] = y
+print(clusterDF.head(3))
+"""
+       ftr1      ftr2  target
+0 -1.692427  3.622025       2
+1  0.697940  4.428867       0
+2  1.100228  4.606317       0
+"""
+
+target_list = np.unique(y)
+# 각 타깃별 산점도의 마커 값
+markers=['o', 's', '^', 'P', 'D', 'H', 'x']
+# 3개의 군집 영역으로 구분한 데이터 세트를 생성했으므로 target_list는 [0, 1, 2]
+# target==0, target==1, target==2 로 scatter plot을 marker별로 생성.
+for target in target_list:
+    target_cluster = clusterDF[clusterDF['target'] == target]
+    plt.scatter(x=target_cluster['ftr1'], y=target_cluster['ftr2'], edgecolor='k',
+                marker=markers[target] )
+
+plt.show()
