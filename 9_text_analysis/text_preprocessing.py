1d4ba2b5c432cb3efc3bf774a65574f7fcfd8dde
soonhankwon <soonable@naver.com>
2026-05-26 22:54:54 +0900

    text preprocessing with nltk

--- 9_text_analysis/text_preprocessing.py
+++ 9_text_analysis/text_preprocessing.py
@@ -15,3 +15,101 @@ print(sentences)
 <class 'list'> 3
 ['The Matrix is everywhere its all around us, here even in this room.', 'You can see it out your window or on your television.', 'You feel it when you go to work, or go to church or pay your taxes.']
 """
+
+from nltk import word_tokenize
+
+sentence = "The Matrix is everywhere its all around us, here even in this room."
+words = word_tokenize(sentence)
+print(type(words), len(words))
+print(words)
+"""
+<class 'list'> 15
+['The', 'Matrix', 'is', 'everywhere', 'its', 'all', 'around', 'us', ',', 'here', 'even', 'in', 'this', 'room', '.']
+"""
+
+from nltk import word_tokenize, sent_tokenize
+
+# 여러개의 문장으로된 입력 데이터를 문장별로 단어 토큰화 만드는 함수 생성
+def tokenize_text(text):
+    # 문장별로 분리 토큰
+    sentences = sent_tokenize(text)
+    # 분리된 문장별 단어 토큰화
+    word_tokens = [word_tokenize(sentence) for sentence in sentences]
+    return word_tokens
+
+# 여러 문장들에 대해 문장별 단어 토큰화 수행
+word_tokens = tokenize_text(text_sample)
+print(type(word_tokens),len(word_tokens))
+print(word_tokens)
+"""
+<class 'list'> 3
+[['The', 'Matrix', 'is', 'everywhere', 'its', 'all', 'around', 'us', ',', 'here', 'even', 'in', 'this', 'room', '.'], ['You', 'can', 'see', 'it', 'out', 'your', 'window', 'or', 'on', 'your', 'television', '.'], ['You', 'feel', 'it', 'when', 'you', 'go', 'to', 'work', ',', 'or', 'go', 'to', 'church', 'or', 'pay', 'your', 'taxes', '.']]
+"""
+
+# ngram
+from nltk import ngrams
+sentence = "The Matrix is everywhere its all around us, here even in this room."
+words = word_tokenize(sentence)
+
+all_ngrams = ngrams(words, 3)
+ngrams = [ngram for ngram in all_ngrams]
+print(ngrams)
+"""
+[('The', 'Matrix', 'is'), ('Matrix', 'is', 'everywhere'), ('is', 'everywhere', 'its'), ('everywhere', 'its', 'all'), ('its', 'all', 'around'), ('all', 'around', 'us'), ('around', 'us', ','), ('us', ',', 'here'), (',', 'here', 'even'), ('here', 'even', 'in'), ('even', 'in', 'this'), ('in', 'this', 'room'), ('this', 'room', '.')]
+"""
+
+# Stopword 제거
+nltk.download('stopwords')
+print('영어 stop words 갯수:', len(nltk.corpus.stopwords.words('english')))
+print(nltk.corpus.stopwords.words('english')[:20])
+"""
+영어 stop words 갯수: 198
+['a', 'about', 'above', 'after', 'again', 'against', 'ain', 'all', 'am', 'an', 'and', 'any', 'are', 'aren', "aren't", 'as', 'at', 'be', 'because', 'been']
+"""
+
+stopwords = nltk.corpus.stopwords.words('english')
+all_tokens = []
+# 위 예제의 3개의 문장별로 얻은 word_tokens list 에 대해 stop word 제거 Loop
+for sentence in word_tokens:
+    filtered_words=[]
+    # 개별 문장별로 tokenize된 sentence list에 대해 stop word 제거 Loop
+    for word in sentence:
+        #소문자로 모두 변환합니다. 
+        word = word.lower()
+        # tokenize 된 개별 word가 stop words 들의 단어에 포함되지 않으면 word_tokens에 추가
+        if word not in stopwords:
+            filtered_words.append(word)
+    all_tokens.append(filtered_words)
+    
+print(all_tokens)
+"""
+[['matrix', 'everywhere', 'around', 'us', ',', 'even', 'room', '.'], ['see', 'window', 'television', '.'], ['feel', 'go', 'work', ',', 'go', 'church', 'pay', 'taxes', '.']]
+"""
+
+# Stemming과 Lemmatization
+from nltk.stem import LancasterStemmer
+stemmer = LancasterStemmer()
+
+print(stemmer.stem('working'),stemmer.stem('works'),stemmer.stem('worked'))
+print(stemmer.stem('amusing'),stemmer.stem('amuses'),stemmer.stem('amused'))
+print(stemmer.stem('happier'),stemmer.stem('happiest'))
+print(stemmer.stem('fancier'),stemmer.stem('fanciest'))
+"""
+work work work
+amus amus amus
+happy happiest
+fant fanciest
+"""
+
+from nltk.stem import WordNetLemmatizer
+nltk.download('wordnet')
+
+lemma = WordNetLemmatizer()
+print(lemma.lemmatize('amusing','v'),lemma.lemmatize('amuses','v'),lemma.lemmatize('amused','v'))
+print(lemma.lemmatize('happier','a'),lemma.lemmatize('happiest','a'))
+print(lemma.lemmatize('fancier','a'),lemma.lemmatize('fanciest','a'))
+"""
+amuse amuse amuse
+happy happy
+fancy fancy
+"""
