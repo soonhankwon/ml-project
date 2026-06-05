7d358a6bacdf836ecada3cf5fa25c83e2335852e
soonhankwon <soonable@gmail.com>
2026-06-05 10:32:53 +0900

    document clustering

--- 9_text_analysis/document_clustering.py
+++ 9_text_analysis/document_clustering.py
@@ -73,3 +73,87 @@ print(document_df.head())
 
 [5 rows x 3 columns]
 """
+
+document_df[document_df['cluster_label'] == 0].sort_values(by='filename')
+document_df[document_df['cluster_label'] == 1].sort_values(by='filename')
+document_df[document_df['cluster_label'] == 2].sort_values(by='filename')
+document_df[document_df['cluster_label'] == 3].sort_values(by='filename')
+document_df[document_df['cluster_label'] == 4].sort_values(by='filename')
+
+from sklearn.cluster import KMeans
+
+# 3개의 군집으로 군집화
+km_cluster = KMeans(n_clusters=3, max_iter=10000, random_state=0)
+km_cluster.fit(feature_vect)
+cluster_label = km_cluster.labels_
+
+# 소속 클러스터를 cluster_label 컬럼으로 할당하고 cluster_label 값으로 정렬
+document_df['cluster_label'] = cluster_label
+document_df.sort_values(by='cluster_label')
+
+# 군집(Cluster)별 핵심 단어 추출하기
+cluster_centers = km_cluster.cluster_centers_
+print('cluster_centers shape :', cluster_centers.shape)
+print(cluster_centers)
+"""
+cluster_centers shape : (3, 4610)
+[[0.         0.00099548 0.00174656 ... 0.         0.00183397 0.00144581]
+ [0.0100545  0.         0.         ... 0.00706288 0.         0.        ]
+ [0.         0.00092552 0.         ... 0.         0.         0.        ]]
+"""
+
+# 군집별 top n 핵심단어, 그 단어의 중심 위치 상대값, 대상 파일명들을 반환함
+def get_cluster_details(cluster_model, cluster_data, feature_names, clusters_num, top_n_features=10):
+    cluster_details = {}
+
+    # cluster_centers array의 값이 큰 순으로 정렬된 index 값을 반환
+    # 군집 중심점(centroid)별 할당된 word 피처들의 거리값이 큰 순으로 값을 구하기 위함
+    centroid_feature_ordered_ind = cluster_model.cluster_centers_.argsort()[:, ::-1]
+
+    # 개별 군집별로 iteration하면서 핵심단어, 그 단어의 중심 위치 상대값, 대상 파일명 입력
+    for cluster_num in range(clusters_num):
+        # 개별 군집별 정보를 담을 데이터 초기화.
+        cluster_details[cluster_num] = {}
+        cluster_details[cluster_num]['cluster'] = cluster_num
+
+        # cluster_centers_.argsort()[:,::-1]로 구한 index를 이용하여 top n 피처 단어를 구함
+        top_feature_indexes = centroid_feature_ordered_ind[cluster_num, :top_n_features]
+        top_features = [feature_names[ind] for ind in top_feature_indexes]
+
+        # top_feature_indexes를 이용해 해당 피처 단어의 중심 위치 상댓값 구함 
+        top_feature_values = cluster_model.cluster_centers_[cluster_num, top_feature_indexes].tolist()
+        
+        # cluster_details 딕셔너리 객체에 개별 군집별 핵심 단어와 중심위치 상대값, 그리고 해당 파일명 입력
+        cluster_details[cluster_num]['top_features'] = top_features
+        cluster_details[cluster_num]['top_features_value'] = top_feature_values
+        filenames = cluster_data[cluster_data['cluster_label'] == cluster_num]['filename']
+        filenames = filenames.values.tolist()
+        cluster_details[cluster_num]['filenames'] = filenames
+
+    return cluster_details
+
+def print_cluster_details(cluster_details):
+    for cluster_num, cluster_detail in cluster_details.items():
+        print(f'####### Cluster {cluster_num}')
+        print('Top features:', cluster_detail['top_features'])
+        print('Reviews 파일명 :',cluster_detail['filenames'][:7])
+        print('==================================================')
+
+feature_names = tfidf_vect.get_feature_names_out()
+cluster_details = get_cluster_details(cluster_model=km_cluster, cluster_data=document_df, feature_names=feature_names, clusters_num=3, top_n_features=10)
+
+print_cluster_details(cluster_details)
+"""
+####### Cluster 0
+Top features: ['room', 'hotel', 'service', 'staff', 'food', 'location', 'bathroom', 'clean', 'price', 'parking']
+Reviews 파일명 : ['room_holiday_inn_london', 'location_holiday_inn_london', 'staff_bestwestern_hotel_sfo', 'service_swissotel_hotel_chicago', 'service_bestwestern_hotel_sfo', 'food_holiday_inn_london', 'staff_swissotel_chicago']
+==================================================
+####### Cluster 1
+Top features: ['screen', 'battery', 'keyboard', 'battery life', 'life', 'kindle', 'direction', 'video', 'size', 'voice']
+Reviews 파일명 : ['battery-life_ipod_nano_8gb', 'voice_garmin_nuvi_255W_gps', 'speed_garmin_nuvi_255W_gps', 'size_asus_netbook_1005ha', 'screen_garmin_nuvi_255W_gps', 'battery-life_amazon_kindle', 'satellite_garmin_nuvi_255W_gps']
+==================================================
+####### Cluster 2
+Top features: ['interior', 'seat', 'mileage', 'comfortable', 'gas', 'gas mileage', 'transmission', 'car', 'performance', 'quality']
+Reviews 파일명 : ['gas_mileage_toyota_camry_2007', 'comfort_honda_accord_2008', 'interior_toyota_camry_2007', 'transmission_toyota_camry_2007', 'seats_honda_accord_2008', 'mileage_honda_accord_2008', 'quality_toyota_camry_2007']
+==================================================
+"""
